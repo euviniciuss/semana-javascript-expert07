2febac29d63d519ed35b8715f764f468aa54f3d5
euviniciuss <dev.mvsc@gmail.com>
2023-03-02 10:17:19 -0300

    refactor: abstraindo funcionalidade de filtro utilizando web workers

--- src/pages/titles/src/factories/cardsFactory.js
+++ src/pages/titles/src/factories/cardsFactory.js
@@ -4,20 +4,16 @@ import CardsService from "./../services/cardsService.js"
 
 const cardListWorker = new Worker(`./src/workers/cardListWorker.js`, { type: 'module' })
 
-cardListWorker.onmessage = (msg) => {
-  console.log("processo principal", msg)
-}
-
-cardListWorker.postMessage('Hey message!')
-
 const [rootPath] = window.location.href.split('/pages/')
 
 const factory = {
   async initialize() {
     return CardsController.initialize({
-      worker: cardListWorker,
       view: new CardsView(),
-      service: new CardsService({ dbUrl: `${rootPath}/assets/database.json` })
+      service: new CardsService({ 
+        dbUrl: `${rootPath}/assets/database.json`,
+        cardListWorker
+      })
     })
   }
 }
